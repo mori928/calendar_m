61e10e0c5033271ac5a47e4515ead7851f67489b
mori928 <mori.nutrition@gmail.com>
2023-09-28 07:27:36 +0900

    Update yoyaku.rb

--- app/models/yoyaku.rb
+++ app/models/yoyaku.rb
@@ -3,13 +3,12 @@ class Yoyaku < ApplicationRecord
   default_scope -> { where(deleted_at: nil) }
 
   # ソフトデリートのメソッド
-  def soft_delete
-    update(deleted_at: Time.current)
-  end
+  # def soft_delete
+  #   update(deleted_at: Time.current)
+  # end
 
-
-  belongs_to :guest, optional: true
-  belongs_to :user, optional: true
+  has_many :bookings
+  belongs_to :user
   
   validates :title,  presence: true #, allow_blank: true
   validates :start_time,  presence: true #, allow_blank: true
